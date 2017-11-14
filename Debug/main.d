2b3cfa93ae8416a0217c5ff494c0009e39887663
Victor Parra <pvictor3@gmail.com>
2017-11-14 16:20:28 -0600

    Correccion en la configuracion del PWM, faltaba setear el enable del registro PWM3CTL

--- Debug/main.d
+++ Debug/main.d
@@ -8,6 +8,7 @@ main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/tm4c123gh6pm.h
 main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h
 main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h
 main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/rom.h
 
 ../main.c: 
 C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.3.LTS/include/stdint.h: 
@@ -17,3 +18,4 @@ C:/ti/TivaWare_C_Series-2.1.4.178/inc/tm4c123gh6pm.h:
 C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h: 
 C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h: 
 C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/rom.h: 
--- main.c
+++ main.c
@@ -8,15 +8,20 @@
 #include"inc/hw_types.h"
 #include"driverlib/sysctl.h"
 
+#define TARGET_IS_BLIZZARD_RB1
+#include "driverlib/rom.h"
+
 int main(void)
 {
-	SysCtlClockSet(SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN); //System clock = 40MHZ
+	ROM_SysCtlClockSet(SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN); //System clock = 40MHZ
 
 	SYSCTL->RCGC2 |= (1<<5);    //Activa el reloj para GPIOF, PF2 y PF3 como M1PWM6 y M1PWM7
 	SYSCTL->RCGCPWM = (1<<1);   //Activa pwm module 1
 
+	GPIOF->DIR = (1<<3)|(1<<2); //PF2 y PF3 como salidas
 	GPIOF->AFSEL |= (1<<2)|(1<<3);  //Alternate function para PF2 y PF3
 	GPIOF->PCTL |= (5<<8)|(5<<12);  //Codigo para usar como PWM
+	GPIOF->DEN = (1<<3)|(1<<2);     //Digital Enable
 	SYSCTL->RCC |= (1<<20);         //Activa el divisor del system clock para el PWM
 	SYSCTL->RCC &= ~(7<<17);         //Pone al divisor en 2
 
@@ -28,7 +33,7 @@ int main(void)
 	PWM1->_3_CMPA = 0x4E1;          //Duty Cycle al 50%
 	PWM1->_3_CMPB = 0x752;          //Duty Cycle al 25%
 
-	PWM1->CTL = (1<<3);             //Globalsync generador 3
+	PWM1->_3_CTL = (1<<0);             //PWM block enable
 	PWM1->ENABLE = (1<<7)|(1<<6);   //Activa la salida para PWM6 y PWM7
 
 	while(1);
