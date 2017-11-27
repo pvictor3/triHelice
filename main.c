554b979aa535ea5c27c60e59b39aa8eadeb2bce1
Victor Parra <pvictor3@gmail.com>
2017-11-27 13:28:51 -0600

    Se cambio la funcion readData por updateData que solo actualiza la DCM, los sensores se leen continuamente

--- main.c
+++ main.c
@@ -24,10 +24,10 @@
 #define TARGET_IS_BLIZZARD_RB1
 #include "driverlib/rom.h"
 
+#define MPU9250_ADDRESS 0x68
+#define MAG_ADDRESS     0x0C
+
 void UARTConfig(void);
-uint_fast8_t MPU9250Init(tI2CMInstance *psI2CInst, uint_fast8_t ui8I2CAddr);
-uint_fast8_t MPU9250Read(uint_fast8_t ui8Reg, uint8_t *pui8Data, uint_fast16_t ui16Count);
-uint_fast8_t MPU9250Write(uint8_t *pui8Data, uint_fast16_t ui16Count);
 
 uint32_t ultimoValorCanal2 = 0, ultimoValorCanal3 = 0, ultimoValorCanal4 = 0, ultimoValorCanal5 = 0;
 uint32_t tiempo2, tiempo3, tiempo4, tiempo5;
@@ -50,7 +50,7 @@ uint32_t g_ui32SchedulerNumTasks = 1;
 // Prototypes of functions which will be called by the scheduler.
 //
 //*****************************************************************************
-static void readData(void *pvParam);
+static void updateDCM(void *pvParam);
 
 //*****************************************************************************
 //
@@ -61,9 +61,9 @@ static void readData(void *pvParam);
 tSchedulerTask g_psSchedulerTable[] =
 {
 //
-// Read data every 2 ticks (50 per second).
+// Update data every 2 ticks (50 per second).
 //
-{ readData, (void *)0, 2, 0, true},
+{ updateDCM, (void *)0, 2, 0, true},
 };
 
 //*****************************************************************************
@@ -115,13 +115,16 @@ if(ui8Status != I2CM_STATUS_SUCCESS)
 g_bI2CMSimpleDone = true;
 }
 uint8_t pfData_int[14];
+uint8_t pfData_int_Mag[6];
+
+float pfData[12];
+float *pfAccel, *pfGyro, *pfMag, *pfEulers;
+int i;
+
+
+
 int main(void)
 {
-
-    float pfData[12];
-    float *pfAccel, *pfGyro, *pfMag, *pfEulers;
-    int i;
-
     //
     // Initialize convenience pointers that clean up and clarify the code
     // meaning. We want all the data in a single contiguous array so that
@@ -135,9 +138,20 @@ int main(void)
     //System clock = 40MHZ
 	ROM_SysCtlClockSet(SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN);
 
+    //
+    // Enable processor interrupts.
+    //
+    ROM_IntMasterEnable();
+
+	//
+	// Initialize the task scheduler and configure the SysTick to interrupt
+	// 100 times per second(Every 10ms).
+	//
+	SchedulerInit(TICKS_PER_SECOND);
+
 	//Activa los periféricos a utilizar
 	SYSCTL->RCGC2 |= (1<<5)|(1<<4)|(1<<3)|(1<<2)|(1<<0);        //Activa el reloj para GPIOA, GPIOC, GPIOD, GPIOE,GPIOF
-	SYSCTL->RCGCPWM |= (1<<0);                           //Activa el reloj para PWM MODULE 1 y MODULE 0
+	SYSCTL->RCGCPWM |= (1<<0);                                  //Activa el reloj para PWM MODULE 0
 	SYSCTL->RCGCWTIMER |= (1<<0);                               //Activa el timer 0 de 32bits
 	SYSCTL->RCGCI2C |= (1<<1);                                  //Activa I2C modulo 1
 
@@ -156,7 +170,7 @@ int main(void)
 	UARTprintf("PWM Iniciado \n");
 
 	//
-	//Inicializa el GPIOA para generar una interrupcion en ambos flancos
+	//Inicializa el GPIOA para generar una interrupcion en ambos flancos y leer el receiver
 	//
 	gpioAConfig();
 	UARTprintf("GPIOA Iniciado \n");
@@ -177,8 +191,6 @@ int main(void)
     timerConfig();
     UARTprintf("Timer Iniciado \n");
 
-
-
     //Configura el I2C
     ROM_GPIOPinConfigure(GPIO_PA6_I2C1SCL);
     ROM_GPIOPinConfigure(GPIO_PA7_I2C1SDA);
@@ -186,23 +198,28 @@ int main(void)
     ROM_GPIOPinTypeI2CSCL(GPIO_PORTA_BASE, GPIO_PIN_6);
     ROM_GPIOPinTypeI2C(GPIO_PORTA_BASE, GPIO_PIN_7);
 
-    //
-    // Enable processor interrupts.
-    //
-    ROM_IntMasterEnable();
-
     I2CMInit(&g_sI2CInst, I2C1_BASE, INT_I2C1, 0xff, 0xff, SysCtlClockGet());
     UARTprintf("I2C Iniciado \n");
 
     //MPU9250 Reset
     for(i = 0; i < 14; i++){
-        pfData_int[i] = 3;
+        pfData_int[i] = 0;
     }
     pui8Buffer[0] = 0x6B;   //PWR_MGMT_1 address
     pui8Buffer[1] = 0x80;   //Reset
-    I2CMWrite(&g_sI2CInst, 0x68, pui8Buffer, 2, I2CMSimpleCallback, 0);
-    while(!g_bI2CMSimpleDone);
+    I2CMWrite(&g_sI2CInst, MPU9250_ADDRESS, pui8Buffer, 2, I2CMSimpleCallback, 0);
+    //while(!g_bI2CMSimpleDone);
     g_bI2CMSimpleDone = false;
+
+    UARTprintf("Acelerometro y Giroscopio Iniciado \n");
+
+    //Magnetometro Mode 3 (100Hz)
+    pui8Buffer[0] = 0x0A;   //CNTL1
+    pui8Buffer[1] = 0x06;   //Continuos measurement mode 2 (100hz)
+    I2CMWrite(&g_sI2CInst, MAG_ADDRESS, pui8Buffer, 2, I2CMSimpleCallback, 0);
+    //while(!g_bI2CMSimpleDone);
+    g_bI2CMSimpleDone = false;
+    UARTprintf("Magnetometro iniciado \n");
 
     //
     // Initialize the DCM system. 50 hz sample rate.
@@ -210,189 +227,86 @@ int main(void)
     //
     CompDCMInit(&g_sCompDCMInst, 1.0f / 50.0f, 0.2f, 0.6f, 0.2f);
     ui32CompDCMStarted = 0;
+    UARTprintf("DCM Iniciado \n");
 
+    //Activa las tareas a ejecutar por el scheduler
     //
-    // Initialize the task scheduler and configure the SysTick to interrupt
-    // 100 times per second.
-    //
-    SchedulerInit(TICKS_PER_SECOND);
-
     SchedulerTaskEnable(0,true);
+    UARTprintf("Scheduler Iniciado \n");
 
 
-    UARTprintf("Inicializacion completa!!!");
+    UARTprintf("Inicializacion completa!!! \n");
+    UARTprintf("Tiempo: %dms", SchedulerTickCountGet()*10);
 	while(1){
-
 	    //
 	    // Tell the scheduler to call any periodic tasks that are due to be
 	    // called.
 	    //
 	    SchedulerRun();
+/*
+	    //Read sensors data Acel and Gyro
+	    pui8Buffer[0] = 0x3B;   //ACCEL_XOUT_H
+	    I2CMRead(&g_sI2CInst, MPU9250_ADDRESS, pui8Buffer, 1, pfData_int, 14,
+	                 I2CMSimpleCallback, 0);
+	    while(!g_bI2CMSimpleDone);
+	    g_bI2CMSimpleDone = false;
 
-	    UARTprintf("acX = %d \n", (pfData_int[0] << 8) | pfData_int[1]);
-	    UARTprintf("acY = %d \n", (pfData_int[2] << 8) | pfData_int[3]);
-	    UARTprintf("acZ = %d \n", (pfData_int[4] << 8) | pfData_int[5]);
-	    UARTprintf("temp = %d \n", (pfData_int[6] << 8) | pfData_int[7]);
-	    UARTprintf("gyX = %d \n", (pfData_int[8] << 8) | pfData_int[9]);
-	    UARTprintf("gyY = %d \n", (pfData_int[10] << 8) | pfData_int[11]);
-	    UARTprintf("gyZ = %d \n", (pfData_int[12] << 8) | pfData_int[13]);
+	    //Read Magneto
+	    uint8_t ST1;
+	    pui8Buffer[0] = 0x02;   //Status 1
+	    do{
+	        I2CMRead(&g_sI2CInst, MAG_ADDRESS, pui8Buffer, 1, &ST1, 1,
+	                 I2CMSimpleCallback,0);
+	    }while( !(ST1 & 0x01) ); //Data ready?
+	    g_bI2CMSimpleDone = false;
 
-	    //
-	    // Check if this is our first data ever.
-	    //
-	    if(ui32CompDCMStarted == 0)
-	    {
-	        //
-	        // Set flag indicating that DCM is started.
-	        // Perform the seeding of the DCM with the first data set.
-	        //
-	        ui32CompDCMStarted = 1;
-	        CompDCMMagnetoUpdate(&g_sCompDCMInst, pfMag[0], pfMag[1],
-	                             pfMag[2]);
-	        CompDCMAccelUpdate(&g_sCompDCMInst, pfAccel[0], pfAccel[1],
-	                           pfAccel[2]);
-	        CompDCMGyroUpdate(&g_sCompDCMInst, pfGyro[0], pfGyro[1],
-	                          pfGyro[2]);
-	        CompDCMStart(&g_sCompDCMInst);
-	    }
-	    else
-	    {
-	        //
-	        // DCM Is already started.  Perform the incremental update.
-	        //
-	        CompDCMMagnetoUpdate(&g_sCompDCMInst, pfMag[0], pfMag[1],
-	                             pfMag[2]);
-	        CompDCMAccelUpdate(&g_sCompDCMInst, pfAccel[0], pfAccel[1],
-	                           pfAccel[2]);
-	        CompDCMGyroUpdate(&g_sCompDCMInst, -pfGyro[0], -pfGyro[1],
-	                          pfGyro[2]);
-	        CompDCMUpdate(&g_sCompDCMInst);
-	    }
+	    pui8Buffer[0] = 0x03;
+	    I2CMRead(&g_sI2CInst, MAG_ADDRESS, pui8Buffer, 1, pfData_int_Mag, 6,
+	             I2CMSimpleCallback, 0);
+	    while(!g_bI2CMSimpleDone);
+	    g_bI2CMSimpleDone = false;
 
+	    uint8_t status2;
+	    pui8Buffer[0] = 0x09;
+	    I2CMRead(&g_sI2CInst, MAG_ADDRESS, pui8Buffer, 1, &status2, 1,
+	             I2CMSimpleCallback, 0);
+	    while(!g_bI2CMSimpleDone);
+	    g_bI2CMSimpleDone = false;*/
 	}
 }
 
-static void readData(void *pvParam){
-    pui8Buffer[0] = 0x3B;   //ACCEL_XOUT_H
-    I2CMRead(&g_sI2CInst, 0x68, pui8Buffer, 1, pfData_int, 14,
-             I2CMSimpleCallback, 0);
-    while(!g_bI2CMSimpleDone);
-    g_bI2CMSimpleDone = false;
-}
-
-//*****************************************************************************
-//
-//! Writes data to MPU9250 registers.
-//!
-//! \param ui8Reg is the first register to write.
-//! \param pui8Data is a pointer to the data to write.
-//! \param ui16Count is the number of data bytes to write.
-//!
-//! This function writes a sequence of data values to consecutive registers in
-//! the MPU9250.  The first byte of the \e pui8Data buffer contains the value
-//! to be written into the \e ui8Reg register, the second value contains the
-//! data to be written into the next register, and so on.
-//!
-//! \return Returns 1 if the write was successfully started and 0 if it was
-//! not.
-//
-//*****************************************************************************
-uint_fast8_t
-MPU9250Write(uint8_t *pui8Data,
-             uint_fast16_t ui16Count)
-{
+static void updateDCM(void *pvParam){
     //
-    // Write the requested registers to the MPU9250.
+    // Check if this is our first data ever.
     //
-    if(I2CMWrite(&g_sI2CInst, MPU9150_I2C_ADDRESS,
-                  pui8Data, ui16Count, I2CMSimpleCallback, 0) == 0)
-    {
-        return(0);
-    }
-
-    //
-    // Success.
-    //
-    return(1);
-}
-
-//*****************************************************************************
-//
-//! Reads data from MPU9250 registers.
-//!
-//! \param ui8Reg is the first register to read.
-//! \param pui8Data is a pointer to the location to store the data that is
-//! read.
-//! \param ui16Count is the number of data bytes to read.
-//!
-//! This function reads a sequence of data values from consecutive registers in
-//! the MPU9250.
-//!
-//! \return Returns 1 if the write was successfully started and 0 if it was
-//! not.
-//
-//*****************************************************************************
-uint_fast8_t
-MPU9250Read(uint_fast8_t ui8Reg, uint8_t *pui8Data,
-            uint_fast16_t ui16Count)
-{
-    //
-    // Read the requested registers from the MPU9150.
-    //
-    pui8Buffer[0] = ui8Reg;
-    if(I2CMRead(&g_sI2CInst, MPU9150_I2C_ADDRESS,
-                pui8Buffer, 1, pui8Data, ui16Count,
-                I2CMSimpleCallback, 0) == 0)
+    if(ui32CompDCMStarted == 0)
     {
         //
-        // The I2C write failed return a
-        // failure.
+        // Set flag indicating that DCM is started.
+        // Perform the seeding of the DCM with the first data set.
         //
-
-        return(0);
+        ui32CompDCMStarted = 1;
+        CompDCMMagnetoUpdate(&g_sCompDCMInst, pfMag[0], pfMag[1],
+                             pfMag[2]);
+        CompDCMAccelUpdate(&g_sCompDCMInst, pfAccel[0], pfAccel[1],
+                           pfAccel[2]);
+        CompDCMGyroUpdate(&g_sCompDCMInst, pfGyro[0], pfGyro[1],
+                          pfGyro[2]);
+        CompDCMStart(&g_sCompDCMInst);
     }
-
-    //
-    // Success.
-    //
-    return(1);
-}
-
-//*****************************************************************************
-//
-//! Initializes the MPU9250 driver.
-//!
-//! \param psI2CInst is a pointer to the I2C master driver instance data.
-//! \param ui8I2CAddr is the I2C address of the MPU9250 device.
-//! \param pfnCallback is the function to be called when the initialization has
-//! completed (can be \b NULL if a callback is not required).
-//! \param pvCallbackData is a pointer that is passed to the callback function.
-//!
-//! This function initializes the MPU9250 driver, preparing it for operation.
-//!
-//! \return Returns 1 if the MPU9250 driver was successfully initialized and 0
-//! if it was not.
-//
-//*****************************************************************************
-uint_fast8_t
-MPU9250Init(tI2CMInstance *psI2CInst,
-            uint_fast8_t ui8I2CAddr)
-{
-    //
-    // Load the buffer with command to perform device reset
-    //
-    pui8Buffer[0] = MPU9150_O_PWR_MGMT_1;
-    pui8Buffer[1] = MPU9150_PWR_MGMT_1_DEVICE_RESET;
-    if(I2CMWrite(psI2CInst, ui8I2CAddr,
-                 pui8Buffer, 2, I2CMSimpleCallback, 0) == 0)
+    else
     {
-        return(0);
+        //
+        // DCM Is already started.  Perform the incremental update.
+        //
+        CompDCMMagnetoUpdate(&g_sCompDCMInst, pfMag[0], pfMag[1],
+                             pfMag[2]);
+        CompDCMAccelUpdate(&g_sCompDCMInst, pfAccel[0], pfAccel[1],
+                           pfAccel[2]);
+        CompDCMGyroUpdate(&g_sCompDCMInst, -pfGyro[0], -pfGyro[1],
+                          pfGyro[2]);
+        CompDCMUpdate(&g_sCompDCMInst);
     }
-
-    //
-    // Success
-    //
-    return(1);
 }
 
 void UARTConfig(void){
